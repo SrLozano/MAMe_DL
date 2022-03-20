f4878352b53589aa5d00220780f711aa5ea9096b
SrLozano <100383511@alumnos.uc3m.es>
2022-03-20 22:23:10 +0100

    Confusion matrix 📈

--- mame_cnn.py
+++ mame_cnn.py
@@ -7,9 +7,13 @@ import pandas as pd
 
 import matplotlib
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.metrics import classification_report, confusion_matrix
+import numpy as np
 
 
-def sort_dataset_folder(execute_image_sorting_bool):
+def sort_dataset_folder(execute_image_sorting_bool, metadata_info):
     """
     This function sorts the dataset folder so that it can be read using generators.Thus, the folder structure is:
         data_256
@@ -28,21 +32,20 @@ def sort_dataset_folder(execute_image_sorting_bool):
         │   │   └── 1914.383.jpg
         │   └── Ceramic
         │       └── 1914.537.jpg
+    :param metadata_info: dataframe with metadata from the dataset
     :param execute_image_sorting_bool: boolean to control whether the dataset folder gets sorted
     :return:
     """
 
-    metadata = pd.read_csv("dataset/MAMe_metadata/MAMe_dataset.csv")
-
     if execute_image_sorting_bool:
 
-        subset_names = list(metadata['Subset'].unique())
+        subset_names = list(metadata_info['Subset'].unique())
 
         for i in subset_names:
             os.makedirs(os.path.join('dataset/data_256', i))
 
         for c in subset_names:
-            for i in list(metadata[metadata['Subset'] == c]['Image file']):
+            for i in list(metadata_info[metadata_info['Subset'] == c]['Image file']):
 
                 # Create path to the image
                 get_image = os.path.join('dataset/data_256', i)
@@ -52,7 +55,7 @@ def sort_dataset_folder(execute_image_sorting_bool):
                     # Move the image
                     move_image_to_cat = shutil.move(get_image, 'dataset/data_256/' + c)
 
-        classes_names = list(metadata['Medium'].unique())
+        classes_names = list(metadata_info['Medium'].unique())
 
         for subset in ['train', 'val', 'test']:
 
@@ -60,7 +63,7 @@ def sort_dataset_folder(execute_image_sorting_bool):
                 os.makedirs(os.path.join(f'dataset/data_256/{subset}/', i))
 
             for c in classes_names:
-                aux_df = metadata.loc[(metadata['Medium'] == c) & (metadata['Subset'] == subset)]
+                aux_df = metadata_info.loc[(metadata_info['Medium'] == c) & (metadata_info['Subset'] == subset)]
                 for index, row in aux_df.iterrows():
 
                     # Create path to the image
@@ -121,20 +124,43 @@ def create_plots(history_plot):
     plt.savefig('MAMe_loss.pdf')
 
 
+def create_confusion_matrix(model_cf, test_generator, metadata_info):
+    """
+    This functions creates a confusion matrix for the test set
+    :param model_cf: model to evaluate
+    :param test_generator: image generator corresponding to test set
+    :param metadata_info: metadata of the dataset
+    :return:
+    """
+    # Compute probabilities
+    y_pred_aux = model_cf.predict(test_generator)
+
+    # Assign most probable label
+    y_pred = np.argmax(y_pred_aux, axis=1)
+
+    # target_names = sorted(list(metadata_info['Medium'].unique()))
+    cf = confusion_matrix(np.argmax(test_generator, axis=1), y_pred)
+    plt.figure(figsize=(15, 12))
+    sns.heatmap(cf, annot=True, cmap="Blues")
+    plt.savefig('MAMe_confusion_matrix.pdf')
+
+
 if __name__ == "__main__":
 
     print("Hello")
 
     # Variable declaration
     execute_image_sorting = False
-    verbose_level = 2  # 0: Silent, 1: Minimum detail, 2: Maximum detail
+    verbose_level = 1  # 0: Silent, 1: Minimum detail, 2: Maximum detail
 
     matplotlib.use('Agg')  # Select the backend used for rendering and GUI integration.
     if verbose_level == 2:
         print('Using Keras version', keras.__version__)  # Display Keras version
 
+    metadata = pd.read_csv("dataset/MAMe_metadata/MAMe_dataset.csv")
+
     # Sort dataset so that it can be read with image generators
-    sort_dataset_folder(execute_image_sorting)
+    sort_dataset_folder(execute_image_sorting, metadata)
 
     # Load dataset
     train_it, val_it, test_it = load_dataset()
@@ -161,12 +187,12 @@ if __name__ == "__main__":
     model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.001, momentum=0.9),
                   loss='categorical_crossentropy', metrics=['accuracy'])
 
-    # Fit Network
+    # Fit network
     history = model.fit(
         x=train_it,
-        epochs=10,
+        epochs=3,
         validation_data=val_it,
-        verbose=2)
+        verbose=0)
 
     # Test model
     test_lost, test_acc = model.evaluate(test_it)
@@ -175,3 +201,22 @@ if __name__ == "__main__":
 
     # Create plots
     create_plots(history)
+
+    print("Create files")
+    '''
+    # Create txt file with important information about network performance
+    id_experiment = "000_experiment"
+    f = open(f"{id_experiment}.txt", "w+")
+    f.write(f"Experiment {id_experiment} \n Test Accuracy: {test_acc}")
+
+    target_names = sorted(list(metadata['Medium'].unique()))
+    y_pred_aux = model.predict(test_it)
+    y_pred = np.argmax(y_pred_aux, axis=1)
+
+    f.write(classification_report(np.argmax(test_it, axis=1), y_pred, target_names=target_names))
+    f.close()'''
+
+    print("Confusion Matrix")
+
+    '''# Plot confusion  matrix
+    create_confusion_matrix(model, test_it, metadata)'''
